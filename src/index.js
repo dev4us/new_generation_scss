5c1eabe55de1f32b970e8edf763835b4836e417b
dev4us <bronxofsoul@gmail.com>
2020-05-05 21:37:19 +0900

    3_Main_Axis_and_Cross_Axis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import App from "./1_Life_Before_Flexbox";
-import App from "./2_First_Rule_of_Flexbox";
+//import App from "./2_First_Rule_of_Flexbox";
+import App from "./3_Main_Axis_and_Cross_Axis";
 
 ReactDOM.render(
   <React.StrictMode>
