72c195f56083d652e04ab853f75c98e4290aa092
dev4us <bronxofsoul@gmail.com>
2020-05-05 21:26:15 +0900

    2_First_Rule_of_Flexbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./1_Life_Before_Flexbox";
+//import App from "./1_Life_Before_Flexbox";
+import App from "./2_First_Rule_of_Flexbox";
 
 ReactDOM.render(
   <React.StrictMode>
